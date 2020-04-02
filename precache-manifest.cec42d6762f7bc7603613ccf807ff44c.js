467a0a8f3b8f99ae58940f65d4c31a1d2790119d
kbsjsn <shin.kirk@gmail.com>
2020-04-01 21:46:35 -0700

    Updates

---
+++ precache-manifest.cec42d6762f7bc7603613ccf807ff44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6399beeb6e7454585c61c119d57317d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e843129ff0b07ac8909c",
+    "url": "/static/css/main.d1cd7fcd.chunk.css"
+  },
+  {
+    "revision": "699ce12285dcfae3e77e",
+    "url": "/static/js/2.9b1e6010.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9b1e6010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e843129ff0b07ac8909c",
+    "url": "/static/js/main.e92e62e0.chunk.js"
+  },
+  {
+    "revision": "4eca9fc36b40029e6d07",
+    "url": "/static/js/runtime-main.fb48cbce.js"
+  }
+]);
